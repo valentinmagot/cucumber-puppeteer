1be8e3c5116daf9f3c479b94fc911a76b4dd77ca
Valentin Magot <MagotV@ad.pwgsc-tpsgc.gc.ca>
2019-04-29 15:57:37 -0400

    tried fix audit again

--- features/support/action/clickElement.js
+++ features/support/action/clickElement.js
@@ -2,7 +2,7 @@
  * Clicks on an item.
  * @param {String} selector CSS selector of the item to click.
  * @param {String} targetedSelector If not null, the link click triggers a navigation that the 
- * test should allow to complete and wait for the target selector to be loaded.
+ * test should allow to complete and wait for the target selector to be loaded. It timesout if the targeted element is not found. Possible to set timout.
  */
 module.exports = async function(selector, targetedSelector) {
     // Element causes a navigation that we need to wait for (link click, form submit)
